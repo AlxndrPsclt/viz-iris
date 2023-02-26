b324a874553b4434e86b7fd3636eed85952293e8
alxndrpsclt <alxndr.psclt@gmail.com>
2023-02-26 01:36:38 +0100

    Adds random to lines potions and angles

--- sketch.js
+++ sketch.js
@@ -2,9 +2,24 @@ let xoff = 0;
 
 let currentRotation=0;
 
+var line1X=300;
+var line1Y=-150;
+var line1Angle=3*PI/4;
+
+var line2X=300;
+var line2Y=-150;
+var line2Angle=3*PI/4;
+
+var line3X=300;
+var line3Y=-150;
+var line3Angle=3*PI/4;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(24);
+  centerX=windowWidth/2;
+  centerY=windowHeight/2;
+  //createCanvas(1000, 1000);
+  frameRate(20);
   background('rgba(0%,0%,0%,1)');
   stroke(255);
   noFill();
@@ -39,29 +54,47 @@ function drawSquaresLine(xStart, yStart, numberOfSquares, angle) {
 function draw() {
   // Draw the flat line at the beginning
   //if (framecount <= width/3) {
-  background('rgba(0%,0%,0%,0.04)');
+  background('rgba(0%,0%,0%,0.025)');
+  translate(centerX,centerY);
 
-  if (random()>0.6) {
-    drawSquaresLine(200, 200, 10, currentRotation);
+  rotate(currentRotation);
+
+  if (frameCount % 100 == 0) {
+    console.log("GO");
+    console.log(frameCount);
+    line1X=int(random()*500) - 250;
+    line1Y=int(random()*500) - 250;
+    line1Angle=random()*2*PI-PI;
   }
 
-  if (random()>0.6) {
-    drawSquaresLine(200, 200, 15, currentRotation+PI/4);
+  if (frameCount % 70 == 0) {
+    console.log("GO2");
+    console.log(frameCount);
+    line2X=int(random()*500) - 250;
+    line2Y=int(random()*500) - 250;
+    line2Angle=random()*2*PI-PI;
   }
 
-  if (random()>0.6) {
-    drawSquaresLine(200, 200, 20, currentRotation+PI/2);
+  if (frameCount % 80 == 0) {
+    console.log("GO3");
+    console.log(frameCount);
+    line3X=int(random()*500) - 250;
+    line3Y=int(random()*500) - 250;
+    line3Angle=random()*2*PI-PI;
   }
 
-  if (random()>0.6) {
-    drawSquaresLine(200, 200, 30, currentRotation+3*PI/4);
+  if (random()>0.1) {
+    drawSquaresLine(line1X, line1Y, 30+int(random(8)), line1Angle);
   }
 
-  if (random()>0.6) {
-    drawSquaresLine(200, 200, 40, currentRotation+4*PI/5);
+  if (random()>0.1) {
+    drawSquaresLine(line2X, line2Y, 32+int(random(8)), line2Angle);
+  }
+  if (random()>0.1) {
+    drawSquaresLine(line3X, line3Y, 40+int(random(8)), line3Angle);
   }
 
-  currentRotation -= PI/80;
+  currentRotation -= PI/160;
   //drawSquaresLine(20, 100, 10, 30, 20);
 }
 
